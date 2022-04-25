547739c9453caf9439f2ec690fea601bc00821df
ChenXiao Liu <810196813@qq.com>
2022-04-24 23:52:35 -0500

    replace normal scripts with es6 modules

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,50 +1,48 @@
-window.addEventListener('DOMContentLoaded', () => {
-  let operator = '+';
-  const message = document.getElementById('message');
-  const text1 = document.getElementById('num1');
-  const text2 = document.getElementById('num2');
+let operator = '+';
+const message = document.getElementById('message');
+const text1 = document.getElementById('num1');
+const text2 = document.getElementById('num2');
 
-  function calculate() {
-    if (text1.value === '' || text2.value === '') {
-      message.innerText = '';
-      return;
-    }
-    const num1 = Number(text1.value);
-    const num2 = Number(text2.value);
-    message.className = 'error';
-
-    if (Number.isNaN(num1) || Number.isNaN(num2)) {
-      message.innerText = 'Invalid Input';
-      return;
-    }
-    let result;
-
-    switch (operator) {
-      case '+':
-        result = num1 + num2;
-        break;
-      case '-':
-        result = num1 - num2;
-        break;
-      case '*':
-        result = num1 * num2;
-        break;
-      default:
-        if (num2 === 0) {
-          message.innerText = 'Divisor should not be 0';
-          return;
-        }
-        result = num1 / num2;
-    }
-    message.className = 'normal';
-    message.innerText = `${num1} ${operator} ${num2} = ${result}`;
+function calculate() {
+  if (text1.value === '' || text2.value === '') {
+    message.innerText = '';
+    return;
   }
-  text1.addEventListener('keyup', calculate);
-  text2.addEventListener('keyup', calculate);
-  document.getElementsByName('operator').forEach((element) => {
-    element.addEventListener('change', () => {
-      operator = element.id;
-      calculate();
-    });
+  const num1 = Number(text1.value);
+  const num2 = Number(text2.value);
+  message.className = 'error';
+
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    message.innerText = 'Invalid Input';
+    return;
+  }
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    default:
+      if (num2 === 0) {
+        message.innerText = 'Divisor should not be 0';
+        return;
+      }
+      result = num1 / num2;
+  }
+  message.className = 'normal';
+  message.innerText = `${num1} ${operator} ${num2} = ${result}`;
+}
+text1.addEventListener('keyup', calculate);
+text2.addEventListener('keyup', calculate);
+document.getElementsByName('operator').forEach((element) => {
+  element.addEventListener('change', () => {
+    operator = element.id;
+    calculate();
   });
 });
--- scripts/weather.js
+++ scripts/weather.js
@@ -1,34 +1,32 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const temperature = document.getElementsByClassName('weather-temp')[0];
-  const humidity = document.getElementsByClassName('weather-humidity')[0];
-  const location = document.getElementsByClassName('weather-loc')[0];
-  const tomorrowImg = document.getElementsByClassName('weather-tomorrow')[0];
-  const dayafterImg = document.getElementsByClassName('weather-dayaftertomorrow')[0];
+const temperature = document.getElementsByClassName('weather-temp')[0];
+const humidity = document.getElementsByClassName('weather-humidity')[0];
+const location = document.getElementsByClassName('weather-loc')[0];
+const tomorrowImg = document.getElementsByClassName('weather-tomorrow')[0];
+const dayafterImg = document.getElementsByClassName('weather-dayaftertomorrow')[0];
 
-  function getImageURL(code) {
-    return `http://us.yimg.com/i/us/nws/weather/gr/${code}ds.png`;
-  }
+function getImageURL(code) {
+  return `http://us.yimg.com/i/us/nws/weather/gr/${code}ds.png`;
+}
 
-  function fetchWeather() {
-    fetch('https://classes.engineering.wustl.edu/cse330/content/weather_json.php')
-      .then((response) => response.json())
-      .then((data) => {
-        temperature.innerText = data.current.temp;
-        humidity.innerText = data.atmosphere.humidity;
-        const strong = document.createElement('strong');
-        strong.innerText = data.location.city;
-        location.innerHTML = '';
-        location.append(strong);
-        location.append(` ${data.location.state}`);
-        tomorrowImg.src = getImageURL(data.tomorrow.code);
-        dayafterImg.src = getImageURL(data.dayafter.code);
-      })
-      .catch((error) => alert(error));
-  }
-  fetchWeather();
+function fetchWeather() {
+  fetch('https://classes.engineering.wustl.edu/cse330/content/weather_json.php')
+    .then((response) => response.json())
+    .then((data) => {
+      temperature.innerText = data.current.temp;
+      humidity.innerText = data.atmosphere.humidity;
+      const strong = document.createElement('strong');
+      strong.innerText = data.location.city;
+      location.innerHTML = '';
+      location.append(strong);
+      location.append(` ${data.location.state}`);
+      tomorrowImg.src = getImageURL(data.tomorrow.code);
+      dayafterImg.src = getImageURL(data.dayafter.code);
+    })
+    .catch((error) => alert(error));
+}
+fetchWeather();
 
-  const button = document.createElement('button');
-  button.addEventListener('click', fetchWeather);
-  button.innerText = 'Update';
-  document.body.append(button);
-});
+const button = document.createElement('button');
+button.addEventListener('click', fetchWeather);
+button.innerText = 'Update';
+document.body.append(button);
